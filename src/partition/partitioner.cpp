1da7b993412133d181b23211354b48437a1a3fa3
vng <viktor.govako@gmail.com>
2017-07-19 15:32:05 +0300

    Don’t renumber osrm nodes. Breaks offline generator logic.

--- src/partition/partitioner.cpp
+++ src/partition/partitioner.cpp
@@ -179,6 +179,8 @@ int Partitioner::Run(const PartitionConfig &config)
                     << " bit size " << std::ceil(std::log2(level_to_num_cells[level] + 1));
     }
 
+    // This nodes renumbering breaks offline generator tool logic.
+    /*
     TIMER_START(renumber);
     auto permutation = makePermutation(edge_based_graph, partitions);
     renumber(edge_based_graph, permutation);
@@ -207,6 +209,7 @@ int Partitioner::Run(const PartitionConfig &config)
     }
     TIMER_STOP(renumber);
     util::Log() << "Renumbered data in " << TIMER_SEC(renumber) << " seconds";
+    */
 
     TIMER_START(packed_mlp);
     MultiLevelPartition mlp{partitions, level_to_num_cells};
