06a02686c28da89c00bd4f5a334737ac3d8c5b20
vng <viktor.govako@gmail.com>
2017-12-20 19:20:43 +0300

    Common facade alias for online and offline routing.

--- include/engine/datafacade.hpp
+++ include/engine/datafacade.hpp
@@ -2,34 +2,25 @@
 #define OSRM_ENGINE_DATAFACADE_DATAFACADE_HPP
 
 #ifdef OSRM_EXTERNAL_MEMORY
-
 #include "routing/compressed_datafacade.hpp"
-
-namespace osrm
-{
-namespace engine
-{
-
-using DataFacadeBase = datafacade::BaseDataFacade;
-template <typename AlgorithmT> using DataFacade = datafacade::CompressedDataFacadeT<AlgorithmT>;
-}
-}
-
 #else
-
 #include "engine/datafacade/contiguous_internalmem_datafacade.hpp"
+#endif
 
 namespace osrm
 {
 namespace engine
 {
 
+#ifdef OSRM_EXTERNAL_MEMORY
+using DataFacadeBase = datafacade::BaseDataFacade;
+#else
 using DataFacadeBase = datafacade::ContiguousInternalMemoryDataFacadeBase;
+#endif
+
 template <typename AlgorithmT>
 using DataFacade = datafacade::ContiguousInternalMemoryDataFacade<AlgorithmT>;
 }
 }
-
-#endif
 
 #endif
